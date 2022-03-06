63ce960ebabdcbcf1a72e3e098d1243bea5f5616
Anto509XD <75438526+Anto509XD@users.noreply.github.com>
2022-03-06 09:05:31 -0500

    PrimerCommit

---
+++ app/src/main/java/com/example/ejemplolistview/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.ejemplolistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var frutas: ArrayList<String>
+        frutas = ArrayList()
+        frutas.add("Manzana")
+        frutas.add("Sandia")
+        frutas.add("Durazno")
+        frutas.add("Platano")
+
+        val grid: GridView = findViewById(R.id.grid)
+
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
+
+        grid.adapter = adaptador
+
+        grid.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, frutas.get(position), Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
